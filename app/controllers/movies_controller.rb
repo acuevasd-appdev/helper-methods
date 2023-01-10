3813fbcad611dd6564a050441ee0126c50817b49
acuevasd <acuevasd@chicagobooth.edu>
2023-01-10 00:27:38 +0000

    updating routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,6 @@ class MoviesController < ApplicationController
     @the_movie = Movie.new
     render template: "movies/new.html.erb"
 
-
   end
   
   def index
@@ -11,36 +10,51 @@ class MoviesController < ApplicationController
 
     @list_of_movies = matching_movies.order({ :created_at => :desc })
 
-    render({ :template => "movies/index.html.erb" })
+    respond_to do  |format|
+      format.json do
+        render json: @list_of_movies
+      end
+      format.html do
+        render template: "movies/index.html.erb"
+      end
+    end
   end
 
   def show
-    the_id = params.fetch("path_id")
+    the_id = params.fetch(:id)
 
     matching_movies = Movie.where({ :id => the_id })
 
-    @the_movie = matching_movies.at(0)
+    @the_movie = matching_movies.first
 
-    render({ :template => "movies/show.html.erb" })
+    render template: "movies/show.html.erb"
   end
 
   def create
     the_movie = Movie.new
     the_movie.title = params.fetch("query_title")
     the_movie.description = params.fetch("query_description")
-    the_movie.released = params.fetch("query_released")
-
+    
     if the_movie.valid?
       the_movie.save
-      redirect_to("/movies", { :notice => "Movie created successfully." })
     else
-      redirect_to("/movies", { :alert => the_movie.errors.full_messages.to_sentence })
+      redirect_to movies_url, notice: "Movie created successfully."
+      render template: "movies/new.html.erb"
     end
   end
 
+  def edit
+    the_id = params.fetch(:id)
+
+    matching_movies = Movie.where({:id => the_id})
+    @the_movie = matching_movies.first
+
+    render template: "movies/edit.html.erb"
+  end
+
   def update
-    the_id = params.fetch("path_id")
-    the_movie = Movie.where({ :id => the_id }).at(0)
+    the_id = params.fetch(:id)
+    the_movie = Movie.where({ :id => the_id }).first
 
     the_movie.title = params.fetch("query_title")
     the_movie.description = params.fetch("query_description")
@@ -48,9 +62,9 @@ class MoviesController < ApplicationController
 
     if the_movie.valid?
       the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully."} )
+      redirect_to movie_url(the_movie), notice: "Movie updated successfully." 
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => the_movie.errors.full_messages.to_sentence })
+      redirect_to movie_url(the_movie), alert: the_movie.errors.full_messages.to_sentence 
     end
   end
 
@@ -60,6 +74,6 @@ class MoviesController < ApplicationController
 
     the_movie.destroy
 
-    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+    redirect_to movies_url, notice: "Movie deleted successfully." 
   end
 end
